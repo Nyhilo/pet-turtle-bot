5adce49c3a436be4faaab2ad1e272efa2c18874c
nyhilo <silentclowd@gmail.com>
2018-05-07 18:13:07 -0700

    Added syntaxt to prepare for testing, load in sprites and token

--- pet-turtle-bot.py
+++ pet-turtle-bot.py
@@ -1,1 +1,50 @@
-#
+#pet-turtle-bot
+import discord
+from discord.ext import commands
+import sys
+
+NAME = 'Bok Choy'
+
+with open("token",'r') as f:
+    TOKEN = f.read()
+
+with open("ascii_sprite",'r') as f:
+    SPRITE = "".join(f.readlines())
+    print(SPRITE)
+
+
+def main():
+    #Prefix
+    bot = commands.Bot(command_prefix='!', description="A bot for running a community pet keeping game. The app will keep track of a pet's hunger, happiness, and health as well as other stats.")
+
+    @bot.event
+    async def on_ready():
+        print('Logged in as', bot.user.name)
+        print("Discord API version: " + discord.__version__)
+
+    @bot.command()
+    async def hello():
+        """Description of command goes here"""
+        await bot.say('Hi! My name is ' + NAME)
+
+    @bot.command(pass_context=True)
+    async def revise(ctx):
+        """"""
+        await bot.edit_message('Hi! My name is not' + NAME)
+
+    @bot.command()
+    async def turtle():
+        await bot.say(SPRITE)
+
+    bot.run(TOKEN)
+
+  
+
+if __name__ == '__main__':  
+    if len(sys.argv) > 1:
+        if (sys.argv[1].lower() == "-test" or sys.argv[1].lower() == "-t"):
+            print("Testing...")
+        else:
+            main()
+    else:
+        main()
