6ddcb4328e9e54479bdd8179e1ca90584f826805
nyhilo <silentclowd@gmail.com>
2018-05-09 13:31:00 -0700

    format printing complete

--- pet-turtle-bot.py
+++ pet-turtle-bot.py
@@ -2,48 +2,106 @@
 import discord
 from discord.ext import commands
 import sys
+import time
+from datetime import datetime
 
-NAME = 'Bok Choy'
-
+# Vars #
 with open("token",'r') as f:
     TOKEN = f.read()
 
+# Sprite file can contain multiple ascii art sprites, each seperated by a $
 with open("ascii_sprite",'r') as f:
-    SPRITE = "```" + "".join(f.readlines()) + "```"
+    SPRITES = "".join(f.readlines()).split('$')
 
 
-def main():
-    #Prefix
-    bot = commands.Bot(command_prefix='!', description="A bot for running a community pet keeping game. The app will keep track of a pet's hunger, happiness, and health as well as other stats.")
+# Misc Functions #
 
-    @bot.event
-    async def on_ready():
-        print("Python version", sys.version)
-        print(SPRITE[3:-3])
-        print('Logged in as', bot.user.name)
-        print("Discord API version: " + discord.__version__)
 
-    @bot.command(pass_context=True)
-    async def hello():
-        bot.msg = await bot.say('Hi! My name is ' + NAME)
+# Pet Class #
+class Pet(object):
+    """docstring for Pet"""
+    def __init__(self, name, spritelist):
+        self.name = name
+        self.birthday = datetime(year=2017, month=5, day=8)
+        # self.birthday = datetime.utcnow()
+        self.sprite = spritelist
+        self.statSplit = 5
+        self.maxstat = 100
+        self.happiness = 100
+        self.hunger = 100
+        self.health = 100
+        self.status = "Healthy"
+        self.sleep = 100
+        self.growth = 0 #Growth is what sprite is displayed
+        self.growthrate = 1
 
-    @bot.command(pass_context=True)
-    async def revise():
-        await bot.edit_message(bot.msg,'Hi! This message has been edited')
+    def msg(self, string):
+        return "*" + string + "*"
 
-    @bot.command()
-    async def turtle():
-        await bot.say(SPRITE)
+    def logic(self):
+        pass
 
-    bot.run(TOKEN)
+    def feed(self, amount=1):
+        if self.hunger/self.maxstat < .2:
+            msg = ""
+        self.hunger +=30
 
-  
+    def getStatBar(self, value):
+        fill = int(value / self.statSplit)
+        rest = int(self.maxstat/self.statSplit) - fill
+        # return ('█' * fill) + (' ' * rest)
+        return ('|' * fill) + (' ' * rest)
 
-if __name__ == '__main__':  
-    if len(sys.argv) > 1:
-        if (sys.argv[1].lower() == "-test" or sys.argv[1].lower() == "-t"):
-            print("Testing...")
-        else:
-            main()
-    else:
-        main()
+    def calculateAge(self):
+        diff = datetime.utcnow() - self.birthday
+        if diff.days < 14:
+            return str(diff.days) + (" day" if diff.days==1 else " days")
+        elif diff.days < 70:
+            num = int(diff.days/7)
+            return str(num) + (" week" if num==1 else " weeks")
+        elif diff.days < 365:
+            num = int(diff.days/30.5)
+            return str(num) + (" month" if num==1 else " months")
+        else: 
+            num = int(diff.days/364.25)
+            return str(num) + (" year" if num==1 else " years")
+
+    def format(self):
+        outstring = self.sprite[self.growth]
+        outstring += "\n Name: " + self.name + "    Status: " + self.status + "    Age: " + self.calculateAge()
+        outstring += "\n HAP [" + self.getStatBar(self.happiness) + "]    HUNG [" + self.getStatBar(self.hunger) + "]"
+        outstring += "\n SLP [" + self.getStatBar(self.health)    + "]    HLTH [" + self.getStatBar(self.hunger) + "]"
+        return outstring
+
+
+pet = Pet("Bok Choy", SPRITES)
+print(pet.format())
+
+
+# #Prefix
+# bot = commands.Bot(command_prefix='!', description="A bot for running a community pet keeping game. The app will keep track of a pet's hunger, happiness, and health as well as other stats.")
+
+
+# # Bot Functions #
+# @bot.event
+# async def on_ready():
+#     print("Python version", sys.version)
+#     print(SPRITE[3:-3])
+#     print('Logged in as', bot.user.name)
+#     print("Discord API version: " + discord.__version__)
+
+
+
+# @bot.command()
+# async def hello():
+#     bot.msg = await bot.say('Hi! My name is ' + NAME)
+
+# @bot.command()
+# async def revise():
+#     await bot.edit_message(bot.msg,'Hi! This message has been edited')
+
+# @bot.command()
+# async def turtle():
+#     await bot.say(SPRITE)
+
+# bot.run(TOKEN)
