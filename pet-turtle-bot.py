9702c39c78f290669628b48c22391268eb2b090e
nyhilo <silentclowd@gmail.com>
2018-05-09 19:08:16 -0700

    Finished Logic? Now just need server integration

--- pet-turtle-bot.py
+++ pet-turtle-bot.py
@@ -1,7 +1,10 @@
+# Version 1.0.0
+# By Nyhilo
+
 # TODO List
-# * Add gender to pet, format this throughout bot responses
+# * Add gender to pet, format bot responses to reflect this
+# * Move responses to a more readable/editable external file
 # * Add more age-related sprites
-# * Move responses to a more readable external file
 # * Have the pet keep track of what's given to it
 
 
@@ -24,6 +27,7 @@ with open("ascii_sprite", 'r') as f:
     SPRITES = "".join(f.readlines()).split('$')
 
 # Possible responses by category
+# {} is replaced by pet.name when sent to the server
 responses = {
     'happy': ["{} nudges a stone with his snout."],
     'feed':  ["{} gently nibbles the lettuce from your hand.",
@@ -33,7 +37,15 @@ responses = {
               "{} is looking sluggish and doesn't want to move."],
     'sleep': ["{} is currently snoozing, you can type &awake to wake him up.",
               "{} is currently in a deep sleep, you can type &awake to wake him up.",
-              "{} is currently asleep, you can type &awake to wake him up."]
+              "{} is currently asleep, you can type &awake to wake him up."],
+    'awake': ["{} opens his eyes groggily.",
+              "{} doesn't look too happy about being awake.",
+              "{} awakes with a startle. He must've been dreaming.",
+              "{} waddles over to you sleepily."],
+    'treatDead': ["{} has passed on. Medicine can't help him now."],
+    'treatSick': ["{} looks much better now!",
+                  "That was a close one! But {} is starting to look lively again.",
+                  "{} is looking much healthier now, maybe a little hungry though."]
 }
 
 
@@ -43,6 +55,12 @@ def chooseResponse(category):
         return choice(responses[category])
     else:
         return "{} is sitting there, being idle."
+
+def avg(numList):
+    if len(numList) > 0:
+        return sum(numList) / len(numList)
+    else:
+        return None
 
 
 # Pet Class #
@@ -74,18 +92,58 @@ class Pet(object):
         if stat < 0:
             stat = 0;
 
-    def logic(self):
-        pass
+    def getHealth(self):
+        if self.health == 0:
+            return "Dead"
+        elif self.health/self.maxstat < .1:
+            return "Sick"
+        elif self.health/self.maxstat < .6:
+            return "Doing well"
+        else:
+            return "Healthy"
+
+    def tick(self):
+        """
+            Calculations that trigger every update, aging and game logic
+            Game logic is stats calculated from other stats
+        """
+        # Logic items
+        # Before stat changes from time passing is calculated, we want to see how health the turtle is.
+        hapfit = self.happiness/self.maxstat
+        hunfit = self.hunger/self.maxstat
+        slpfit = self.sleep/self.maxstat
+        fitness = avg([hapfit, hunfit, slpfit])
+        healthmod = int((fitness - 0.6)*10)  # Gives a range of -6 to +4
+        statmod(self.health, healthmod)
+        if self.health == 0:
+            self.status = "Dead"
+
+        # Time based items
+        if self.status != "Sleeping" and slpfit < .1:
+            self.status = "Sleeping"
+        if self.sleep >= 100:
+            self.status = self.getHealth()
+        if self.status == "Sleeping":
+            self.statmod(self.sleep, 10)
+            self.statmod(self.health, 2)
+            self.statmod(self.hunger, 1)
+        else:
+            self.statmod(self.sleep, -5)    # Averages to 12 hours of sleep a day.
+
+        hapmod = int(((self.health/self.maxstat) - .8) * 5) # Ranges from -4 to -1
+        self.statmod(self.happiness, hapmod)
+        self.statmod(self.hunger, -2)
+
 
     def feed(self, amount=1):
         if self.hunger/self.maxstat < .1:
             msg = "{} looks so relieved. He was almost starving!"
         if self.hunger >= self.maxstat:  #Pet is allowed to be overfed, but not when already full
             msg = "{} doesn't look very hungry now."
-        elif self.status == "Sick":
+        elif self.status == "Sick" or self.status == "Dead":
             msg = chooseResponse('sick') + " He doesn't seem to want to eat anything."
         else:
-            amountTilFull = (self.maxstat - self.hunger) % 30) + 1
+            amountTilFull = ((self.maxstat - self.hunger) % 30) + 1
             if amount > amountTilFull:
                 msg = "That's way too much! {} only eats what he can handle."
                 amount = amountTilFull
@@ -96,20 +154,38 @@ class Pet(object):
         return self.respond(msg)
 
     def play(self):
-        if self.status == "sick":
+        if self.status == "sick" or self.status == "Dead":
             msg = chooseResponse('sick') + " He doesn't want to play right now."
         elif self.hunger < 5:
             msg = "{} is too hungry to play right now!"
         else:
             self.statmod(self.happiness, 40)
             self.statmod(self.health, 10)
-            self.statmod(self.hunger, -5)
+            self.statmod(self.hunger, -10)
+            self.statmod(self.sleep, -5)
 
             msg = chooseResponse('play')
         return self.respond(msg)
 
+    def wakeup(self):
+        if self.status != "Sleeping":
+            msg = "{} is already awake!"
+        else:
+            self.statmod(self.happiness, -30)
+            msg = chooseResponse('awake')
+        return respond(msg)
+
     def treat(self):
-        pass    # Given medicine
+        if self.status == "Dead":
+            msg = chooseResponse('treatDead')
+        elif self.status == "Sick":
+            self.status = self.getHealth()
+            self.statmod(self.health, self.maxstat * .8)
+            msg = chooseResponse('treatSick')
+        else:
+            msg = "{} doesn't look sick, medicine probably won't do anything."
+        return respond(msg)
+
 
     def getStatBar(self, value):
         if value > self.maxstat:
@@ -144,7 +220,6 @@ class Pet(object):
 
 pet = Pet("Bok Choy", SPRITES)
 print(pet.format())
-print(pet.respond("{} string"))
 
 # #Prefix
 # bot = commands.Bot(command_prefix='!', description="A bot for running a community pet keeping game. The app will keep track of a pet's hunger, happiness, and health as well as other stats.")
